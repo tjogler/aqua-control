18577ba38021c6f18cdeba7ff463d240a624eefe
tjogler <tjogler@gmail.com>
2018-02-28 21:13:55 +0100

    further implementations of power control functions still missing several important functions

--- power_control.py
+++ power_control.py
@@ -7,55 +7,61 @@ import threading
 import time
 import numpy as np
 import utilities as ut
+import RPi.GPIO as GPIO
 from Adafruit_PWM_Servo_Driver import PWM
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-10s) %(message)s',
                     )
+GPIO.setmode(GPIO.BCM)
 
-
-
-class powerProfile(object):
-
-    def __init__(self,profile_type='simple',resolution=60,maxI=1.,maxIM=0.05,srise='8:00:00',sset='20:00:00',mrise='22:00:00',mset='4:00:00'):
-        self.name=profile_type
-        self.resolution=resolution
-        if maxI <=1: #prevent impossible max intensity
-            self.maxI=maxI
-        else:
-            self.maxI=1.
-        self.maxIM=maxIM
-        if self.maxIM>0.1*self.maxI: #prevent to bright moonlight by false settings
-            self.maxIM=0.1*self.maxI
-        self.sunRise=ut.convert_timestr_to_s(srise)
-        self.sunSet=ut.convert_timestr_to_s(sset)
-        self.moonRise=ut.convert_timestr_to_s(mrise)
-        self.moonSet=ut.convert_timestr_to_s(mset)
-        self.calc_intens_profile()
-
-      def calc_intens_profile(self):
-        self.intensProfile=self.calc_intens_profile_sun()+self.calc_intens_profile_moon()
-       
 class powerChannel(object):
 
     def __init__(self,name,gpio,number,**kwargs):
         self.name=name
         self.gpio=gpio
         self.number=number
-        self.on=1 #default is on if not oserwise configured
+        self.state=state #default is on if not oserwise configured
         self.switchTime=switchTime  #time between two state switches, dampens oszilations
         self.counter=0 #coutner for debugg and timing of switching, is used by other programs
+        self.inverted=invert
         
+        GPIO.setup(self.gpio,GPIO.OUT)
+        if self.state==1:
+            GPIO.output(self.gpio,GPIO.HIGH)
+        else:
+            GPIO.output(self.gpio,GPIO.LOW)
+            
     def set_on(self):
         '''
         function that sets a single power channel on
         '''
-       self.on=1
+        if self.inverted:
+            GPIO.output(self.gpio,GPIO.LOW)
+        else:
+            GPIO.output(self.gpio,GPIO.HIGH)
+        self.state=1
+        
     def set_off(self):
         '''
         function that sets a single power channel off
         '''
-        self.on=0
+        if self.inverted:
+            GPIO.output(self.gpio,GPIO.HIGH)
+        else:
+            GPIO.output(self.gpio,GPIO.LOW)
+        self.state=0
+
+    def toggle(self):
+        '''
+        functions that toggles the state of the channel
+        '''
+        if self.state==1:
+            self.state=0
+            self.set_off()
+        else:
+            self.state=1
+            self.set_on()            
         
 class powerModule(object):
     
@@ -70,55 +76,53 @@ class powerModule(object):
         self.on=False#relict not sure if needed
         self.readSump=readSump
         self.power=-1.#relict not sure if needed
-
-    def status():
+        GPIO.setup(self.readTank,GPIO.IN)
+        GPIO.setup(self.readSump,GPIO.IN)
+        
+    def statusRead(self):
+        self.statusTank=GPIO.input(self.readTank)
+        self.statusSump=GPIO.input(self.readSump)
+        logging.debug('Sump state: {}'.format(self.statusSump))
+        logging.debug('Tank state: {}'.format(self.statusTank))
+        
+    def status(self):
+        logging.debug('============================')
+        logging.debug('power module status')
+        logging.debug('============================')
         for c in self.channel:
-            print '============================'
-            print 'power module status'
-            print '============================'
-            print 'Power channel {} at GPIO[{}], plug number [{}] is set to {}'.format(c.name,c.gpio,c.number,c.on)
+            logging.debug('Power channel {} at GPIO[{}], plug number [{}] is set to {}'.format(c.name,c.gpio,c.number,c.state))
 
+        self.statusRead()
+        
 class RunPower(threading.Thread):
 
-    def __init__(self, event,powerModule,lock,data):
+    def __init__(self, event,powerModule,lock,data,frequency=60):
         threading.Thread.__init__(self)
         self.stopped=event
         self.powerModule=powerModule
         self.lock=lock
         self.data=data
-
+        self.frequency=frequency
+        
     def run(self):
-        logging.debug('power control initializing')
+        logging.debug('power control initializing at {}'.format(time.strftime('%H:%M:%S')))
         counter=0n
-        logging.debug('intensity resolution: {}'.format(res))
+        logging.debug('power state: initialzing')
         
-        
-        #calculate index to start light intensity from
         now=ut.convert_timestr_to_s(time.strftime('%H:%M:%S'))
-        counter=int(now/res)+1
-        if counter >=len(self.lamp.channel[0].profileI.intensProfile):
-                counter=0
+
         logging.debug('starting at intensity array  position: {}'.format(counter))
         start=1
-        while not self.stopped.wait(res):
+        while not self.stopped.wait(self.frequency):
             self.lock.acquire()
-            for c in self.lamp.channel:
-                print 'intensity {}: {}'.format(c.name,c.profileI.intensProfile[counter])
-                if counter==0:
-                    if c.profileI.intensProfile[len(c.profileI.intensProfile)-1]!=c.profileI.intensProfile[counter]:
-                         c.set_intens(c.profileI.intensProfile[counter])
-                    self.data.append(c.profileI.intensProfile[counter])
-                elif c.profileI.intensProfile[counter-1]!=c.profileI.intensProfile[counter] or start==1:
-                    print 'setting intensity channel {} to {}'.format(c.name,c.profileI.intensProfile[counter])
-                    c.set_intens(c.profileI.intensProfile[counter])
-                    self.data.append(c.profileI.intensProfile[counter])
-            if start==1:
-                start=0
-            self.lock.release()
-            counter+=1
-            if counter >=len(self.lamp.channel[0].profileI.intensProfile):
-                counter=0
-        logging.debug('light intensity profile stopped')
+            self.powerModule.status()
+            if self.powerModule.statusTank==1:
+                logging.debug('{} ALERT! water level in tank to high!'.format(time.strftime('%H:%M:%S')))
+            
+            if self.powerModule.statusSUMP==1:
+                logging.debug('{} ALERT! water level in to low!'.format(time.strftime('%H:%M:%S')))
+                            
+        logging.debug('{} power control stopped'.format(time.strftime('%H:%M:%S')))
 
             
 
