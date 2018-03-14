4d04e413134b0e5c80b2f7b8666f34d2502b75eb
tjogler <tjogler@gmail.com>
2018-03-14 20:54:38 +0100

    added camera control modul to be implemented in the future

--- camera_control.py
+++ camera_control.py
@@ -2,27 +2,65 @@
 import numpy as np
 import sys,os
 import time
+import datetime
 import logging
 import threading
 from picamera import PiCamera
-import light_control as lc
 
 
-def take_image(path="./",number=0):
-    camera.start_preview()
-    sleep(3)
-    fname=path+'image_{:04d}.jpg'.format(number)
-    camera.capture(fname)
-    camera.stop_preview()
+class camera(object):
+
+    def __init__(self,path="./"):
+        self.path=path
+        self.counter=0
+        self.lastCaptureTime=datetime.datetime.now().replace(microsecond=0)
+        self.camera=PiCamera()
+        
+    def take_image(self)
+        self.camera.start_preview()
+        time.sleep(3)
+        fname=self.path+'image_{}_{:04d}.jpg'.format(time.strftime('%Y%m%d_%H:%M:%S'),self.counter)
+        self.counter+=1
+        self.camera.capture(fname)
+        self.lastCaptureTime=datetime.datetime.now().replace(microsecond=0)
+        self.camera.stop_preview()
 
     
-def make_bright(channel):
-    if type(channel)==list:
-        self.numChannel=len(self.channel)+1
-    else:
-        self.numChannel=1
-        self.channel=[channel]
+    def make_bright(self,channel):
+        #not yet ready to use, this would require cross thread communication not yet implemented
+        if type(channel)==list:
+            self.numChannel=len(self.channel)+1
+        else:
+            self.numChannel=1
+            self.channel=[channel]
+        
+        for c in channel:
+            if "white" in c.name:
+                c.set_intens(1.0)
 
-    for c in channel:
-        if "white" in c.name:
-                
+class RunCamera(threading.Thread):
+
+    def __init__(self, event,camera,lock,frequency=86400):
+        threading.Thread.__init__(self)
+        self.stopped=event
+        self.frequency=frequency
+        self.lock=lock
+        self.camera=camera
+
+    def run(self):
+        
+        logging.debug('camera control initializing at {}'.format(time.strftime('%Y %m %d %H:%M:%S')))
+        
+        now=datetime.datetime.now().replace(microsecond=0)
+
+        while not self.stopped.wait(300):
+            if (now-self.camera.lastCaptureTime)>=self.frequency:
+                try:
+                    self.lock.acquire()
+                    self.camera.take_image()
+                    self.lock.release()
+                except:
+                    self.lock.release()
+                     logging.debug('ERROR: camera control could not take image at {}'.format(time.strftime('%Y %m %d %H:%M:%S')))
+
+        logging.debug('{} camera control stopped'.format(time.strftime('%Y %m %d %H:%M:%S')))
